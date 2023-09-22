a2d6783348b19aab8e2266c239e3d96c28fe7190
irmanaomisihotang <irmanaomi116@gmail.com>
2023-09-22 19:39:44 +0700

    ini tugas php

---
+++ kontak.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kontak saya</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+	<?php
+	$nama = 'Irma Naomi';
+	$kontak = '(+62) 82138046553';
+	$alamat = 'Medan';
+	$instagram = '@irmanaomi16 ';
+	?>
+    <div class="row">
+		<div class="col-md-12">
+			<h3 class="text-center">
+				Hubungi Saya !!
+			</h3> 
+			<address>
+				<strong><?= $nama ?> </strong><br />
+			</address> 
+			</address>
+				<strong><?= $kontak ?></strong><br />
+
+			<address>
+			<address>
+				<strong><?= $alamat?></strong><br />
+			</address> 
+			<address>
+				<strong><?= $instagram ?></strong><br />
+
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
